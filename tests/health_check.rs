6635988278727ba520104bf9931d17e7882047d6
Bruno Paulino <bruno@bpaulino.com>
2024-12-21 22:42:12 -0300

    Handle form-urlencoded POST requests and dummy tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -15,10 +15,59 @@ async fn health_check_test() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=lu%20ana&email=luana%40gmail.com";
+
+    let resp = client
+        .post(format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute req");
+
+    assert_eq!(200, resp.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_for_invalid_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le", "missing email"),
+        ("email=le%40gmail.com", "missing name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_msg) in test_cases {
+        let resp = client
+            .post(format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute req");
+
+        assert_eq!(
+            400,
+            resp.status().as_u16(),
+            "API did not fail with 400 Bad Request when payload was {}",
+            error_msg
+        );
+    }
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to a random port");
     let port = listener.local_addr().unwrap().port();
     let server = zero2prod::run(listener).expect("Failed to bind address");
+
+    #[allow(clippy::let_underscore_future)]
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
